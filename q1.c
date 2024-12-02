2186362272ab46e5a89ece6a2eb4f8f697670881
cedric <cedric.jovenet0@gmail.com>
2024-12-02 09:47:18 +0100

    début code q1

--- q1.c
+++ q1.c
@@ -1,2 +1,20 @@
 #The file to code in #test
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PROMPT "enseash % "
+#define WELCOME_MESSAGE "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
+
+int main() {
+    char command[1024];
+
+    // Display welcome message
+    write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
+
+    while (1) {
+        // Display prompt
+        write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+        fflush(stdout);

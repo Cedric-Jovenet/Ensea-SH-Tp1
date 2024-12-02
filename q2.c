acd1ea9b7c79908c7d89640baf99cc89c20dc390
cedric <cedric.jovenet0@gmail.com>
2024-12-02 11:36:35 +0100

    q2 final

--- q2.c
+++ q2.c
@@ -2,38 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <signal.h>
 
 #define PROMPT "enseash % "
 #define WELCOME_MESSAGE "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
+#define FORTUNE "Today is what happened to yesterday \n"
+
+
 
 int main() {
     char command[1024];
+    int status;
 
     // Display welcome message
     write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
+    fflush(stdout);
 
     while (1) {
         // Display prompt
         write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
-        fflush(stdout);
-       // Read user input
+
+
+
+        // wait for user input 
         if (fgets(command, sizeof(command), stdin) == NULL) {
-            break; // Exit on Ctrl+D
-        }
+            break; 
+        }	
+        
+         // remove '\n' (newline) from the command input
+        command[strcspn(command, "\n")] = 0;	
 
-        // Remove newline character
-        command[strcspn(command, "\n")] = 0;
+        // if the command is fortune
+        if (strcmp(command, "fortune") ==0 )  {
+            write(STDOUT_FILENO, FORTUNE, strlen(FORTUNE));
 
-        if (strcmp(command, "exit") == 0) {
-            write(STDOUT_FILENO, "Bye bye...\n", 11);
-            break; // Exit on "exit"
         }
-
-        // Execute simple command (later updated for arguments)
-        int ret = system(command);
-        if (ret == -1) {
-            perror("enseash");
-        }
+        
+        else{ 
+		execlp(command, command, NULL); //execute the command and write it
+		}
+		
     }
 
     return 0;

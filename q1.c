0c2e6265c7b315ce75b969edb30940747b0f6128
cedric <cedric.jovenet0@gmail.com>
2024-12-02 11:40:58 +0100

    q3 final

--- q1.c
+++ q1.c
@@ -7,7 +7,6 @@
 #define WELCOME_MESSAGE "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
 
 int main() {
-    char command[1024];
 
     // Display welcome message
     write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));

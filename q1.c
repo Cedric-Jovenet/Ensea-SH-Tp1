caf60de78e4244fa358fe9a099801b650beb608b
cedric <cedric.jovenet0@gmail.com>
2024-12-02 10:47:53 +0100

    q1 new code

--- q1.c
+++ q1.c
@@ -12,29 +12,4 @@ int main() {
     // Display welcome message
     write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
 
-    while (1) {
-        // Display prompt
-        write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
-        fflush(stdout);
-       // Read user input
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            break; // Exit on Ctrl+D
-        }
-
-        // Remove newline character
-        command[strcspn(command, "\n")] = 0;
-
-        if (strcmp(command, "exit") == 0) {
-            write(STDOUT_FILENO, "Bye bye...\n", 11);
-            break; // Exit on "exit"
-        }
-
-        // Execute simple command (later updated for arguments)
-        int ret = system(command);
-        if (ret == -1) {
-            perror("enseash");
-        }
-    }
-
-    return 0;
 }

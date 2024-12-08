79a69d7806e4c98a9bbcbbff0d4f6a58e79618ed
ensea <ensea@StudentLab>
2024-12-08 12:42:38 +0100

    Fin question 1

--- q1.c
+++ q1.c
@@ -6,9 +6,17 @@
 #define PROMPT "enseash % "
 #define WELCOME_MESSAGE "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
 
+void WelcomeMessage(){
+    //Display of a welcome message
+    ssize_t ByteWrite = write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
+}
+
+void Prompt(){
+    //Display of a simple prompt
+    ssize_t ByteWrite = write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+}
+
 int main() {
-
-    // Display welcome message
-    write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
-
+    WelcomeMessage();
+    Prompt();
 }

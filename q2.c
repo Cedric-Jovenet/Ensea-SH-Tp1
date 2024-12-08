cdbeee0f92fe9b413ae1eec9ea30ac43fa6db462
ensea <ensea@StudentLab>
2024-12-08 13:15:13 +0100

    Final q2

--- q2.c
+++ q2.c
@@ -10,41 +10,45 @@
 #define WELCOME_MESSAGE "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
 #define FORTUNE "Today is what happened to yesterday \n"
 
+void Command(char *command){
+	pid_t pid = fork();                                  //fork a new process
+    if(pid == -1){
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if(pid == 0){                                 //child process
+        execlp(command, command, (char *)NULL);
+        exit(EXIT_FAILURE);
+    } else {                                             //parent process
+        waitpid(pid, NULL, 0);                           //wait for the child to finish
+    }
+}
 
+void WelcomeMessage(){
+    //Display of a welcome message
+    ssize_t ByteWrite = write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
+}
+
+void Prompt(){
+    //Display of a simple prompt
+    ssize_t ByteWrite = write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+}
 
 int main() {
-    char command[1024];
-    int status;
-
-    // Display welcome message
-    write(STDOUT_FILENO, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE));
-    fflush(stdout);
-
-    while (1) {
-        // Display prompt
-        write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
-
-
-
-        // wait for user input 
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            break; 
-        }	
-        
-         // remove '\n' (newline) from the command input
-        command[strcspn(command, "\n")] = 0;	
+    WelcomeMessage();
+    while(1){
+		Prompt();
+		char userInput[1024];
+		fgets(userInput, sizeof(userInput), stdin);
+		
+		// remove '\n' (newline) from the command input
+        userInput[strcspn(userInput, "\n")] = 0;	
 
         // if the command is fortune
-        if (strcmp(command, "fortune") ==0 )  {
+        if (strcmp(userInput, "fortune") ==0 )  {
             write(STDOUT_FILENO, FORTUNE, strlen(FORTUNE));
-
         }
         
-        else{ 
-		execlp(command, command, NULL); //execute the command and write it
-		}
-		
-    }
-
-    return 0;
+        //execute the entered command
+        Command(userInput);
+	}
 }

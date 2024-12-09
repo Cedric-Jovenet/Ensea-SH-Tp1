28802d8ef55c212a2673b405002292745ae0ecb8
ensea <ensea@StudentLab>
2024-12-09 08:10:12 +0100

    final q2

--- q2.c
+++ q2.c
@@ -8,19 +8,18 @@
 
 #define PROMPT "enseash % "
 #define WELCOME_MESSAGE "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
-#define FORTUNE "Today is what happened to yesterday \n"
 
 void Command(char *command){
-	pid_t pid = fork();                                  //fork a new process
+	pid_t pid = fork();  //fork a new process
     if(pid == -1){
         perror("fork");
         exit(EXIT_FAILURE);
-    } else if(pid == 0){                                 //child process
+    } else if(pid == 0){  //child process
         execlp(command, command, (char *)NULL);
         perror("execlp");
         exit(EXIT_FAILURE);
-    } else {                                             //parent process
-        waitpid(pid, NULL, 0);                           //wait for the child to finish
+    } else {  //parent process
+        waitpid(pid, NULL, 0);  //wait for the child to finish
     }
 }
 
@@ -36,6 +35,7 @@ void Prompt(){
 
 int main() {
     WelcomeMessage();
+    
     while(1){
 		Prompt();
 		char userInput[1024];
@@ -46,12 +46,6 @@ int main() {
 		// remove '\n' (newline) from the command input
         userInput[strcspn(userInput, "\n")] = 0;	
 
-        // if the command is fortune
-        if (strcmp(userInput, "fortune") == 0 )  {
-            write(STDOUT_FILENO, FORTUNE, strlen(FORTUNE));
-            continue;
-        }
-        
         //execute the entered command
         Command(userInput);
 	}
